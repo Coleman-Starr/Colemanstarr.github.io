55076591a08261aafadb012d75cffdf30b7c0015
Coleman Starr <98662508+Coleman-Starr@users.noreply.github.com>
2022-05-21 20:14:56 -0400

    fixed validation

--- required.js
+++ required.js
@@ -1,3 +1,11 @@
-if (document.getElementById("name") == "") {
-    alert('fill in name');
+let btn = document.getElementById("next");
+
+btn.addEventListener('click', event => {
+    requaired();
+});
+
+function requaired() {
+    if (document.getElementById("name") == "") {
+        alert('fill in name');
+    }
 }
